947c6d227960613e96a0baf9dabf7101441770c0
slavik <nastasyakyaroslav>
2018-03-20 18:25:10 +0100

    Added sending checker and autocomplete field (type)

---
+++ elements/chekedSending.js
@@ -0,0 +1,23 @@
+class CSending extends Polymer.Element {
+
+    static get is() {
+        return 'checked-sending'
+    }
+
+    static get properties() {
+        return {
+            flag:{
+                type: Boolean,
+                value: null
+            },
+            status:{
+                type: Boolean,
+                value: null
+            }
+        }
+
+
+    }
+}
+
+customElements.define(CSending.is, CSending);
