e1944c68120ab669a17cb808582b27f0ff7c05e5
slavik <nastasyakyaroslav>
2018-03-22 19:22:45 +0200

    conflict correction

--- elements/linkType.js
+++ elements/linkType.js
@@ -10,23 +10,8 @@ class LType extends Polymer.Element {
                 type: String,
                 value: null,
                 notify: true,
-            },
-            base: {
-                type: String,
-                value: "http://localhost:8080"
-            },
-            apiV1: {
-                type: String,
-                value: "/api/v1/"
-            },
-            autoKey: {
-                type: String,
-                computed: 'computeFullUrl(base, apiV1)'
-            },
+            }
         }
-    }
-    computeFullUrl(base, apiV1) {
-        return base + apiV1 + "types";
     }
 
 }
--- elements/plugin.js
+++ elements/plugin.js
@@ -38,36 +38,23 @@ chrome.tabs.getSelected(null, tab => {
                     type: Object,
                     value: null
                 },
-                userAvatar: {
-                    type: String
-                },
-                loggedIn: {
-                    type: Boolean,
-                    value: null
-                },
-                starRating: {
-                    type: Number,
-                    value: 0
-                },
-                like: {
-                    type: Boolean,
-                    value: null
-                },
-                description: {
+                signInUrl: {
                     type: String,
-                    value: ' '
+                    computed: 'computeFullSignUrl(base, apiV1)'
                 },
-                linkType: {
+                providers: {
+                    type: Array,
+                    value: function () {
+                        return [];
+                    }
+                },
+                getUser: {
                     type: String,
                     value: null
                 },
                 linkTitle: {
                     type: String,
                     value: null
-                },
-                flag: {
-                    type: Boolean,
-                    value: true
                 }
             }
         }
@@ -77,49 +64,27 @@ chrome.tabs.getSelected(null, tab => {
         }
 
         static get observers() {
-            return ['changeFields(fields.*,getFields,starRating,description,like,linkType,linkTitle)']
+            return ['changeFields(fields.*,getFields)']
         }
 
         computeFullUrl(base, url, apiV1) {
             return base + apiV1 + "notes?url=" + (url);
         }
 
-        checkStatus(e) {
-            if (e.detail.status === 200){
-                this.flag = true;
-            }
-        }
         handle(e) {
             const note = e.detail.response.data.data || {};
-            const {
-                description,
-                star,
-                like,
-                type,
-                title,
-                ...noteFields
-            } = note;
-            this.description = description;
-            this.starRating = +star;
-            this.linkType = type;
-            this.linkTitle = title;
-            if ((like !== null) && (like !== undefined)) {
-                this.like = JSON.parse(like);
-            }
-            const fields = Object.keys(noteFields)
+            const fields = Object.keys(note)
                 .map(key => ({
                     key: key,
                     val: note[key]
                 }));
             this.push('fields', ...fields);
             this.getFields = note;
-            Polymer.dom(this.root).removeChild(this.$.loader)
         }
 
         _handleErrorResponse(e) {
-            if(e.detail.request.xhr.status === 404) {
-                this.$.xhr.generateRequest();
-                this.$.getAjax.generateRequest();
+            if (e.detail.request.xhr.status === 404) {
+                this.$.xhr.auto = 'true';
             }
         }
 
@@ -134,38 +99,14 @@ chrome.tabs.getSelected(null, tab => {
         }
 
         changeFields(changed) {
-            this.userAvatar = this.$.signIn.userAvatar;
-            this.loggedIn = this.$.signIn.loggedIn;
-            this.like = this.$.like.like;
-            this.description = this.$.description.description;
-            this.starRating = this.$.starRating.value;
-            this.linkTitle = this.$.title.ltitle;
-            this.linkType = this.$.type.ltype;
             const result = this.fields
                 .reduce((acc, obj) => {
                     acc[obj.key] = obj.val;
                     return acc;
                 }, {});
-            if ((this.description !== ' ') && (this.description !== '')) {
-                result['description'] = this.description;
-            }
-            if (this.like !== null)
-            {
-                result['like'] = JSON.stringify(this.like);
-            }
-            if ((!isNaN(this.starRating)) && (this.starRating !== 0)) {
-                result['star'] = JSON.stringify(this.starRating);
-            }
-            if (this.linkTitle !== ''){
-                result['title'] = this.linkTitle;
-            }
-            if (this.linkType !== ''){
-                result['type'] = this.linkType;
-            }
             this.postFields = JSON.stringify({id: this.fullUrl, data: result});
             if ((this.getFields !== null) && (JSON.stringify(this.getFields) !== JSON.stringify(result))) {
                 this.$.xhr.auto = 'true';
-                this.flag = false;
             }
         }
     }
